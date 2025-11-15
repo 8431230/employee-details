0b574122027b9126d6d11957b742992deb7f3c2e
soumya_84 <bangaragundsoumya@gmail.com>
2025-11-15 12:12:02 +0530

    Create jenkins2.py

---
+++ jenkins2.py
@@ -0,0 +1,13 @@
+
+employee = {
+    "Employee ID": "EMP101",
+    "Name": "John Doe",
+    "Age": 29,
+    "Department": "IT",
+    "Salary": 55000
+}
+
+# Printing details
+print("----- Employee Details -----")
+for key, value in employee.items():
+    print(f"{key}: {value}")
